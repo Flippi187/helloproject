a92086dc1a48d6ad33c28312c03dd58624d594d4
Philipp de Zwart <philipp.de@bulme.at>
2021-10-01 11:52:39 +0100

    cout added

--- hallo.cpp
+++ hallo.cpp
@@ -1,2 +1,13 @@
+// mein Hallo-Programm
 // (c)philipp.de@bulme.at 2021
-// (c)philipp.de@bulme.at 2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world" << endl;
+	
+	return 0;
+}
